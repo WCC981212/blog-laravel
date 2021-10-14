7fe22f52fca396ed4036de7c0d85a240a93b6467
WCC981212 <wongchuiching98@gmail.com>
2021-10-14 18:56:22 +0800

    Middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <ul class="flex items-center">
             @auth
                 <li>
-                    <a href="" class="p-3">Wong Chui Ching</a>
+                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
                 </li>
                 <li>
                     <form action="{{ route('logout') }}" method="post" class="inline">
