fb53f3019f751223ec51108916d0db9e2e0181a6
WCC981212 <wongchuiching98@gmail.com>
2021-10-15 00:47:38 +0800

    Mail

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Mail\PostLiked;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class PostLikeController extends Controller
 {
@@ -14,13 +16,17 @@ class PostLikeController extends Controller
 
     public function store(Post $post, Request $request)
     {
-        if ($post->likedBy($request->user())) {
-            return response(null, 409); //Conflict
-        }
+        // if ($post->likedBy($request->user())) {
+        //     return response(null, 409); //Conflict
+        // }
 
         $post->likes()->create([
             'user_id' => $request->user()->id,
         ]);
+
+        if (!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()) {
+            Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
+        }
         return back();
     }
 
